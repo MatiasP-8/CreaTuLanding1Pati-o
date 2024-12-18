210bc30f091c39570647eaebd2c9163db1c9ae63
matias <patiniomatias80971a@gmail.com>
2024-12-18 16:58:26 -0300

    Entrega N1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import NavBar from './componentes/NavBar/NavBar'
+import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
 
 const App = () => {
   return (
+  <>
     <NavBar/>
+    <ItemListContainer saludo='HOLA MUNDO'></ItemListContainer>
+  </>
   )
 }
 
--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './NavBar.css';
 import CartWidgeet from '../CartWidget/CartWidgeet'
 
 const NavBar = () => {
